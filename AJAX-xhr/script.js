8f610b2f0f5f665bec7d5aa6c6ec5eb54be362ca
Alex Carranza <alexcarranza9889@gmail.com>
2025-04-20 22:00:45 -0600

    Cleaned the code and added comments for readyState

--- AJAX-xhr/script.js
+++ AJAX-xhr/script.js
@@ -2,13 +2,16 @@ const xhr = new XMLHttpRequest();
 
 
 xhr.open('GET', './series.json'); // Make the request to the server
-console.log('data');
 
 xhr.onreadystatechange = function() {
-    console.log('dat');
 
+    // readyState has 5 possible values
+    // - 0: request not initialized
+    // - 1: server connection established
+    // - 2: request received
+    // - 3: processing request
+    // - 4: request finished and response is ready
     if(this.status === 200 && this.readyState === 4) {
-        console.log('data');
         
         const data = JSON.parse(this.responseText);
 
